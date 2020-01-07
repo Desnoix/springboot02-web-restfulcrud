128ca0a5bee79d1ecc25f7d5dc0d3cc65534cf11
Desnoix <gdmz_jqyang@outlook.com>
2020-01-07 17:45:27 +0800

    fist commit

---
+++ src/main/java/com/yjq/springboot/Springboot02WebRestfulcrudApplication.java
@@ -0,0 +1,13 @@
+package com.yjq.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02WebRestfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02WebRestfulcrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjq/springboot/Springboot02WebRestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjq.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot02WebRestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
